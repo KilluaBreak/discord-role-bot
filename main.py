933468af0ab6d7226d1c1fa931adae0bd2d54f41
𝙺𝚒𝚕𝚕𝚞𝚊 <sulthanrabbani771@gmail.com>
2025-06-26 13:11:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,23 +17,29 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Opsi role yang tersedia
-ROLE_OPTIONS = [
+ROLE_OPTIONS1 = [
     discord.SelectOption(label="ras dewa", value="1382008246327312424"),
     discord.SelectOption(label="ras iblis", value="1382008643326574622"),
     discord.SelectOption(label="ras naga", value="1382010013144322128"),
+    discord.SelectOption(label="ras elemental", value="1385923115929833522"),
+    discord.SelectOption(label="ras dwarf", value="1385922167685775401"),
+    discord.SelectOption(label="ras human", value="1385921921677266995"),
+]
+ROLE_OPTIONS2 = [
     discord.SelectOption(label="giveaway ping", value="1383027202970484766"),
     discord.SelectOption(label="partnership ping", value="1383012056353210488"),
     discord.SelectOption(label="partnership event", value="1383671901699702864"),
+    discord.SelectOption(label="promosi ping", value="1383366844450082817"),
+    discord.SelectOption(label="misteri ping", value="1387340859946569808"),
+]
+ROLE_OPTIONS3 = [
     discord.SelectOption(label="pencinta anime", value="1381930147833315398"),
     discord.SelectOption(label="pencinta donghua", value="1381930494316515411"),
     discord.SelectOption(label="pencinta komik", value="1381931492518662214"),
-    discord.SelectOption(label="vocaloid lovers", value="1381931995373768744"),
-    discord.SelectOption(label="art lovers", value="1381932371787386920"),
+    discord.SelectOption(label="pencinta game", value="1382010013144322128","1383003121416011866","1383003591614140477","1383003667178852494","1383003734552084531"),
+    discord.SelectOption(label="pencinta musik", value="1381931995373768744"),
+    discord.SelectOption(label="pencinta gambar", value="1381932371787386920"),
     discord.SelectOption(label="pengamat server", value="1381932623353610240"),
-    discord.SelectOption(label="Free Fire", value="1383003121416011866"),
-    discord.SelectOption(label="Mobile Legends", value="1383003591614140477"),
-    discord.SelectOption(label="PUBG", value="1383003667178852494"),
-    discord.SelectOption(label="Roblox", value="1383003734552084531"),
 ]
 
 
@@ -42,10 +48,24 @@ ROLE_OPTIONS = [
 class RoleDropdown(discord.ui.Select):
     def __init__(self):
         super().__init__(
-            placeholder="Choose Your Roles",
+            placeholder="pick your ras",
             min_values=1,
             max_values=1,
-            options=ROLE_OPTIONS,
+            options=ROLE_OPTIONS1,
+        )
+     def __init__(self):
+        super().__init__(
+            placeholder="pick your ping",
+            min_values=1,
+            max_values=1,
+            options=ROLE_OPTIONS2,
+        )
+     def __init__(self):
+        super().__init__(
+            placeholder="pick your hobby",
+            min_values=1,
+            max_values=1,
+            options=ROLE_OPTIONS3,
         )
 
     async def callback(self, interaction: discord.Interaction):
